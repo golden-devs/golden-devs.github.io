8c9bd272d3e114a85159e74edc0704f611241645
Kev Comedia <kevcomedia@gmail.com>
2019-08-09 21:22:08 +0800

    Initial webpack configuration
    Pretty basic webpack setup. It can now bundle the scss files (including
    the images in url() to boot!). But the images needed by the html itself
    are not yet bundled.
    
    Since the style.css file is dynamically generated, I removed it.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
