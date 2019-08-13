f313b21d8fafe7008126ed5fc9a12fefbc790ac0
Kev Comedia <kevcomedia@gmail.com>
2019-08-13 17:12:38 +0800

    Enable image optimization on production
    I don't quite like sort of duplicating the rule object, but this works.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,22 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 
-module.exports = merge(common, {
+module.exports = merge.smart(common, {
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: false,
+            },
+          },
+        ],
+      },
+    ]
+  }
 });
